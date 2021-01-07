9a2362a7ab3266aff598f41ad2513019ea630692
Bryan Hai Phan <61600744+bryanhaiphanthanh@users.noreply.github.com>
2021-01-07 14:53:33 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import boto3
 from botocore.config import Config
 
 def get_session():
-    return boto3.Session(aws_access_key_id="AKIA3FRGYDLIKFZC54VP", aws_secret_access_key= "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")
+    return boto3.Session(aws_access_key_id="XXXXXXXXXXXXXXXXXXXX", aws_secret_access_key= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXX")
 
 def get_polly_client():
     session = get_session()
@@ -11,12 +11,6 @@ def get_polly_client():
 
 def get_rekonition_client():
     session = get_session()
-    # config = Config(
-    #     retries = dict(
-    #         max_attempts = 10
-    #     )
-    # )
-    # return session.client('rekognition',  'ca-central-1', config = config)
     return session.client('rekognition',  'us-east-2')
 
 def get_s3_client():
